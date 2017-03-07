f76bef35df854b3cfe114997953bee2ea1e4cc71
Anton Te <te.anton@gmail.com>
2017-03-06 21:24:23 -0800

    - add SDL2_ttf, fftw and swscale

--- config.cpp
+++ config.cpp
@@ -13,7 +13,11 @@ Config::Config(int argc, char **argv)
   cflags.push_back("-std=c++1y");
   cflags.push_back("-O3");
   cflags.push_back("-g");
+
   incToPkg["SDL.h"].push_back("sdl2");
+  incToPkg["SDL_ttf.h"].push_back("SDL2_ttf");
+  incToPkg["fftw3.h"].push_back("fftw3");
+  incToPkg["libswscale/swscale.h"].push_back("libswscale");
   {
     auto &inc = incToPkg["libavformat/avformat.h"];
     inc.insert(std::end(inc), { "libavformat", "libavutil", "libavcodec" });

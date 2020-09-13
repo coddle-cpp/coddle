6d36d59d82f73292cf89e061b655e2a574a48be0
Mika Pi <mika@mika.global>
2020-09-13 16:08:12 -0700

    Wait for other job if other job is same job

--- mem_db.cpp
+++ mem_db.cpp
@@ -7,19 +7,30 @@ MemDb &MemDb::instance()
   return db;
 }
 
-std::optional<std::string> MemDb::lookup(uint32_t hash) const
+std::optional<std::string> MemDb::lookup(uint32_t hash)
 {
-  const std::lock_guard<std::mutex> lock(mutex);
-  const auto iter = cache.find(hash);
-  if (iter != std::end(cache))
-    return iter->second;
-  std::ifstream strm(".coddle/" + std::to_string(hash) + ".artifact", std::ios::binary);
-  if (!strm)
-    return std::nullopt;
-  std::ostringstream out;
-  out << strm.rdbuf();
-  cache.emplace(hash, out.str());
-  return out.str();
+  std::unique_lock<std::mutex> lock(mutex);
+  for (;;)
+  {
+    const auto iter = cache.find(hash);
+    if (iter != std::end(cache))
+      return iter->second;
+    std::ifstream strm(".coddle/" + std::to_string(hash) + ".artifact", std::ios::binary);
+    if (!strm)
+    {
+      if (wip.find(hash) != std::end(wip))
+      {
+        cv.wait(lock);
+        continue;
+      }
+      wip.insert(hash);
+      return std::nullopt;
+    }
+    std::ostringstream out;
+    out << strm.rdbuf();
+    cache.emplace(hash, out.str());
+    return out.str();
+  }
 }
 
 void MemDb::insert(uint32_t hash, const std::string &serOut)
@@ -28,4 +39,6 @@ void MemDb::insert(uint32_t hash, const std::string &serOut)
   cache.emplace(hash, serOut);
   std::ofstream strm(".coddle/" + std::to_string(hash) + ".artifact", std::ios::binary);
   strm << serOut;
+  wip.erase(hash);
+  cv.notify_all();
 }
--- mem_db.hpp
+++ mem_db.hpp
@@ -1,20 +1,24 @@
 #pragma once
 #include "MurmurHash3.h"
 #include "ser.hpp"
+#include <condition_variable>
 #include <mutex>
 #include <optional>
 #include <sstream>
 #include <unordered_map>
+#include <unordered_set>
 
 class MemDb
 {
 public:
   static MemDb &instance();
 
-  std::optional<std::string> lookup(uint32_t hash) const;
+  std::optional<std::string> lookup(uint32_t hash);
   void insert(uint32_t hash, const std::string &serOut);
 
 private:
-  mutable std::unordered_map<uint32_t, std::string> cache;
-  mutable std::mutex mutex;
+  std::unordered_map<uint32_t, std::string> cache;
+  std::unordered_set<uint32_t> wip;
+  std::mutex mutex;
+  std::condition_variable cv;
 };
